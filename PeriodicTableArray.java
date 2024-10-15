6ec0751d26de925ffeb737b7055a85c947e93df4
aceko10 <sangyongko6@gmail.com>
2024-10-15 14:07:16 -0400

    Add files via upload

---
+++ PeriodicTableArray.java
@@ -0,0 +1,141 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+import java.util.Scanner; 
+import java.util.ArrayList; 
+
+public class Main {
+
+  public static void main(String[] args) {
+		ArrayList<String> pTable = new ArrayList<String>();
+
+		pTable.add("H");
+		pTable.add("He");
+		pTable.add("Li");
+		pTable.add("Be");
+		pTable.add("B");
+		pTable.add("C");
+		pTable.add("N");
+		pTable.add("O");
+		pTable.add("F");
+		pTable.add("Ne");
+		pTable.add("Na");
+		pTable.add("Mg");
+		pTable.add("Al");
+		pTable.add("Si");
+		pTable.add("P");
+		pTable.add("S");
+		pTable.add("Cl");
+		pTable.add("Ar");
+		pTable.add("K");
+		pTable.add("Ca");
+		pTable.add("Sc");
+		pTable.add("Ti");
+		pTable.add("V");
+		pTable.add("Cr");
+		pTable.add("Mn");
+		pTable.add("Fe");
+		pTable.add("Co");
+		pTable.add("Ni");
+		pTable.add("Cu");
+		pTable.add("Zn");
+		pTable.add("Ga");
+		pTable.add("Ge");
+		pTable.add("As");
+		pTable.add("Se");
+		pTable.add("Br");
+		pTable.add("Kr");
+		pTable.add("Rb");
+		pTable.add("Sr");
+		pTable.add("Y");
+		pTable.add("Zr");
+		pTable.add("Nb");
+		pTable.add("Mo");
+		pTable.add("Tc");
+		pTable.add("Ru");
+		pTable.add("Rh");
+		pTable.add("Pd");
+		pTable.add("Ag");
+		pTable.add("Cd");
+		pTable.add("In");
+		pTable.add("Sn");
+		pTable.add("Sb");
+		pTable.add("Te");
+		pTable.add("I");
+		pTable.add("Xe");
+		pTable.add("Cs");
+		pTable.add("Ba");
+		pTable.add("La");
+		pTable.add("Ce");
+		pTable.add("Pr");
+		pTable.add("Nd");
+		pTable.add("Pm");
+		pTable.add("Sm");
+		pTable.add("Eu");
+		pTable.add("Gd");
+		pTable.add("Tb");
+		pTable.add("Dy");
+		pTable.add("Ho");
+		pTable.add("Er");
+		pTable.add("Tm");
+		pTable.add("Yb");
+		pTable.add("Lu");
+		pTable.add("Hf");
+		pTable.add("Ta");
+		pTable.add("W");
+		pTable.add("Re");
+		pTable.add("Os");
+		pTable.add("Ir");
+		pTable.add("Pt");
+		pTable.add("Au");
+		pTable.add("Hg");
+		pTable.add("Tl");
+		pTable.add("Pb");
+		pTable.add("Bi");
+		pTable.add("Po");
+		pTable.add("At");
+		pTable.add("Rn");
+		pTable.add("Fr");
+		pTable.add("Ra");
+		pTable.add("Ac");
+		pTable.add("Th");
+		pTable.add("Pa");
+		pTable.add("U");
+		pTable.add("Np");
+		pTable.add("Pu");
+		pTable.add("Am");
+		pTable.add("Cm");
+		pTable.add("Bk");
+		pTable.add("Cf");
+		pTable.add("Es");
+		pTable.add("Fm");
+		pTable.add("Md");
+		pTable.add("No");
+		pTable.add("Lr");
+		pTable.add("Rf");
+		pTable.add("Db");
+		pTable.add("Sg");
+		pTable.add("Bh");
+		pTable.add("Hs");
+		pTable.add("Mt");
+		pTable.add("Ds");
+		pTable.add("Rg");
+		pTable.add("Cn");
+		pTable.add("Nh");
+		pTable.add("Fl");
+		pTable.add("Mc");
+		pTable.add("Lv");
+		pTable.add("Ts");
+		pTable.add("Og");
+		
+		
+		
+	}
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+
+}
